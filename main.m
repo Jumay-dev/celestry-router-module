6460ddc75b1bd610b337bf4e81032e6cd4be1581
Jumaydev <osipovm33rus@gmail.com>
2021-06-12 21:41:16 +0300

    main fix

--- main.m
+++ main.m
@@ -124,7 +124,7 @@ for m = 1:sattelitesCount
 end
 
 modelTact = 200; % imitation beats count
-packagesFromEarthByTact = 46;
+packagesFromEarthByTact = 84;
 pksk = zeros(1, modelTact);
 ppk = zeros(1, modelTact);
 p = 4000000;
